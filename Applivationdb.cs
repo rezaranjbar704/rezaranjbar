9bf6811aef367f3f2fa94b64433cbb0c26bf8659
rezaranjbar704 <63051796+rezaranjbar704@users.noreply.github.com>
2020-04-17 06:01:54 -0700

    Add files via upload

---
+++ Applivationdb.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp1
+{
+    public class Applivationdb : DbContext
+    {
+        public DbSet<Student> Students { get; set ; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(LoggerFactory.Create(configure: builder =>
+                    builder.AddConsole()))
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(
+                    connectionString: @"Server=(localdb)\MSSQLLocalDB;Database=EfCoredb;Integrated security = true;Trusted_Connection=True;ConnectRetryCount=0");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+
+namespace ConsoleApp1
+{
+    
+    
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbInit();
+            Create(name:"Reza.Ranjbar",email:"rezaranjbar704@gmail.com");
+            Read(1);
+            Upadte(1 , newName: "Reza Ranjbar" , newEmail: "rezaranjbar704");
+            Delete(1);
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void Delete(int id)
+        {
+            using (Applivationdb db = new Applivationdb())
+            {
+                Student stu = db.Students.Find(id);
+                db.Students.Remove(stu);
+            }
+        }
+
+        private static void Upadte(int id , string newEmail , string newName)
+        {
+            using (Applivationdb db = new Applivationdb())
+            {
+                Student stu = db.Students.Find(id);
+                stu.Name = newName;
+                stu.Email = newEmail;
+                db.SaveChanges();
+            }
+        }
+
+        private static void Read(int Id)
+        {
+            using (Applivationdb db = new Applivationdb())
+            {
+                Student stu = db.Students.Find(Id);
+                Console.WriteLine(stu);
+            }
+        }
+
+        private static void DbInit()
+        {
+            using (Applivationdb db = new Applivationdb())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+        }
+        
+        public static void Create(string name , string email)
+        {
+            using (Applivationdb db = new Applivationdb())
+            {
+                Student student = new Student() {Name = name, Email = email};
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
